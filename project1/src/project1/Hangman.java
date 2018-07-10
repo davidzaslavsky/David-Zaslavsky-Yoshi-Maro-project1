3eb543e9df5223afeef39a85d675d4cab53cf97a
zaslavd <zaslavd@cf162-18.cs.wwu.edu>
2018-07-10 11:34:53 -0700

    working on project 1

--- project1/src/project1/Hangman.java
+++ project1/src/project1/Hangman.java
@@ -1,5 +1,84 @@
 package project1;
+import java.util.*;
 
 public class Hangman {
 
+	public static void main(String[]args) {
+		
+		while(true) {
+			String difficulty = "";
+			int diffLevel;
+			String secretWord = "";
+			System.out.println("Enter your difficulty: Easy (e), Intermediate (i), or Hard (h)");           //creates a random word and sets the difficulty level
+			Scanner sc = new Scanner(System.in);
+			difficulty = sc.nextLine();
+			diffLevel = difficulty(difficulty);
+			RandomWord word = new RandomWord();
+			secretWord = word.newWord();
+			System.out.println("the secret word is " + secretWord);
+			
+			if(diffLevel == 1) {
+				easyGame(secretWord);
+			}
+			
+		}
+	}
+	
+	
+	
+	public static int difficulty(String d) {
+		
+		int result = 0;
+		
+		for(int i = 0;i < d.length();i++) {
+			if(d.charAt(i) == 'e') {
+				result = 1;
+			}else if(d.charAt(i) == 'i') {
+				result = 2;
+			}else if(d.charAt(i) == 'h') {
+				result = 3;
+			}
+		}
+		
+		return result;
+	}
+	
+	public static int easyGame(String secretWord) {
+		int numGuesses = 0;
+		char[] wordArr = new char[secretWord.length()]; 
+		String[] dashedArr = new String[wordArr.length];
+		
+		for(int i = 0; i < wordArr.length;i++) {
+			wordArr[i] = secretWord.charAt(i);
+		}
+		for(int i = 0; i < dashedArr.length;i++) {
+			dashedArr[i] = "- ";
+		}		
+		
+		while(numGuesses < 15) {
+			String letterGuess;
+			String spacesGuess;
+			
+			Scanner sc = new Scanner(System.in);
+			Scanner sc2 = new Scanner(System.in);
+			
+			System.out.print("Please enter he letter you want to guess: ");
+			letterGuess = sc.next();
+			System.out.println("Please enter the spaces you want to check (seperated by spaces):");   
+			spacesGuess = sc2.nextLine();
+			String[] spacesArr = spacesGuess.split(" ");			
+			
+			for(int i = 0; i < spacesArr.length;i++) {
+				System.out.println(spacesArr[i]);
+			}
+		
+			System.out.print("the word is: ");
+			for(int i = 0; i < dashedArr.length;i++) {
+				System.out.print(dashedArr[i]);
+			}
+			System.out.println("");
+			numGuesses = numGuesses + 20;
+		}
+		return 1;
+	}
 }
